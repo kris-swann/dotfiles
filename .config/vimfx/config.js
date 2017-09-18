81bd0c8619c309fd2f084502cee872dc29b4d0f9
kriswithank <kristopherswann@gmail.com>
2017-09-17 21:34:10 -0500

    Add vimfx config

---
+++ .config/vimfx/config.js
@@ -0,0 +1,4 @@
+vimfx.set('mode.normal.tab_select_next', '<Tab>')
+vimfx.set('mode.normal.tab_select_previous', '<S-Tab>')
+vimfx.set('mode.normal.history_back', '<C-o>')
+vimfx.set('mode.normal.history_forward', '<C-i>')
