d130d1b184b1fa808d77b45e30cf4ed28deb0f7a
kriswithank <kristopherswann@gmail.com>
2018-03-12 22:44:18 -0500

    Remove config for vimfx
    Plugin is now depricated and I no longer use it.

--- .config/vimfx/config.js
+++
@@ -1,4 +0,0 @@
-vimfx.set('mode.normal.tab_select_next', '<Tab>')
-vimfx.set('mode.normal.tab_select_previous', '<S-Tab>')
-vimfx.set('mode.normal.history_back', '<C-o>')
-vimfx.set('mode.normal.history_forward', '<C-i>')
