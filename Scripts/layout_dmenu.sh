1eb4a947bb2026ec5a8aae5adb315cce4700c155
kriswithank <kristopherswann@gmail.com>
2019-10-20 14:26:26 -0500

    Some cleanup to layout chooser menu

--- Scripts/layout_dmenu.sh
+++ Scripts/layout_dmenu.sh
@@ -1,1 +1,1 @@
-ls -1 ~/.screenlayout | sed -e 's/.sh//g' -e 's/_/ /g' | dmenu | sed -e 's/ /_/g' -e 's/$/.sh/' -e 's/^/~\/.screenlayout\//' | bash
+ls -1 ~/.screenlayout | sed -e 's/.sh//g' -e 's/_/ /g' | dmenu -i -p "Layout Profile" | sed -e 's/ /_/g' -e 's/$/.sh/' -e 's/^/~\/.screenlayout\//' | bash
