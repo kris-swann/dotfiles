c9a9ee922c502d5f2480d0b83a43ca3e646f1d95
kriswithank <kristopherswann@gmail.com>
2019-10-20 15:11:58 -0500

    Move all layout scripts to dmenu script

--- Scripts/layout_dmenu.sh
+++ Scripts/layout_dmenu.sh
@@ -1,1 +1,9 @@
-ls -1 ~/.screenlayout | sed -e 's/.sh//g' -e 's/_/ /g' | dmenu -i -p "Layout Profile" | sed -e 's/ /_/g' -e 's/$/.sh/' -e 's/^/~\/.screenlayout\//' | bash
+options=$(ls -1 ~/.screenlayout | sed -e 's/.sh//g' -e 's/_/ /g' -e '$ a Manual (arandr)')
+result=$(echo "$options" | dmenu -i -p "Layout Profile")
+
+if [ "$result" == 'Manual (arandr)' ]; then
+  arandr
+elif [[ "$result" ]]; then
+  layout_file_path=$(echo "$result" | sed -e 's/ /_/g' -e 's/$/.sh/' -e 's/^/~\/.screenlayout\//')
+  (exec "$layout_file_path")
+fi
