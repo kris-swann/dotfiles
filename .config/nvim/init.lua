d33e665a27190a1be25408091238ccc7303900b1
kris-swann <krisswann@fastmail.com>
2022-11-02 20:09:33 -0600

    Change pcall so can easily enable profiling

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,5 @@
 -- For faster startup times, this should go as close to top as possible (may not have been installed yet, so use pcall)
-pcall(require, 'impatient')
+pcall(function() require('impatient') end)
 
 require('kris')
 require('pkgs.packer')
