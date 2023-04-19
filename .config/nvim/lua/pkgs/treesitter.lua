3b5aba5889e164bc72c322343099d3d2704ad189
kris-swann <krisswann@fastmail.com>
2023-04-18 19:21:36 -0600

    Disable treesitter for kotlin, it was giving me a hard time

--- .config/nvim/lua/pkgs/treesitter.lua
+++ .config/nvim/lua/pkgs/treesitter.lua
@@ -3,6 +3,7 @@ require('nvim-treesitter.configs').setup({
   ignore_install = {'smali'},
   highlight = {
     enable = true,
+    disable = { 'kotlin' },
     -- additional_vim_regex_highlighting = false,
     -- additional_vim_regex_highlighting = { "markdown" },
     -- disable = { 'markdown' }
